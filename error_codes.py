29f7b8fa3077376126353fafcc9258a181490904
AntonGrudkin <antongrudkin8@gmail.com>
2016-06-07 01:25:36 +0300

    Checking if pdf file opened in external program added.  It is implemented with function check_output_filename(filename : string) : string, witch is returning possible to compile in TeX processor filename without closing of recently opened files.
    File error_codes.py contains function error_code_message witch returns error message by Windows error code. This function is necessary to use when Windows standard error message has not ascii encoding and it may be displayed incorrectly.
    
     Also small changes in code style and interface.

---
+++ error_codes.py
@@ -0,0 +1,10 @@
+def error_code_message(err=0, filename=''):
+    """string"""
+    if err == 0:
+        return "Success"
+    elif err == 2:
+        return "File " + filename + " not found"
+    elif err == 32:
+        return "File " + filename + " opened in another program"
+    else:
+        return "Error" + str(err)
--- interface1.py
+++ interface1.py
@@ -3,14 +3,43 @@ from tkinter.messagebox import showinfo
 from polygen import *
 import os
 import subprocess
+from error_codes import *
 
 __author__ = 'Anton Grudkin'
 
-head_file = open("head.tex", 'r')
-ground_file = open("ground.tex", 'r')
+head_file = open("tex\head.tex", 'r')
+ground_file = open("tex\ground.tex", 'r')
 
-problems = open("problems.tex", 'w')
-answers = open("answers.tex", 'w')
+
+def check_output_filename(filename_input):
+    """string"""
+    filename = filename_input
+    copy = 0
+    while True:
+        try:
+            os.unlink(filename + '.pdf')
+            break
+        except BaseException as err:
+            error_code = int(format(err).split(']')[0].split(' ')[1])
+            showinfo(title='popup', message=error_code_message(error_code, filename + ".pdf"))
+            copy += 1
+            if copy == 1:
+                filename += '(' + format(copy) + ')'
+            else:
+                filename.replace('(' + format(copy - 1) + ')', '(' + format(copy) + ')')
+            if error_code == 2:
+                break
+            print(error_code)
+    # print (filename)
+    return filename
+
+answers_filename = check_output_filename('answers')
+problems_filename = check_output_filename('problems')
+
+answers = open(answers_filename + '.tex', 'w')
+problems = open(problems_filename + '.tex', 'w')
+
+open_files_flag = BooleanVar
 
 
 def head_writer():
@@ -34,6 +63,8 @@ def polygen_writer(count=10, sum_count=5, deg=4, cof=20):
 
 
 def generate_pdf(filename):
+
+    # print(filename)
     cmd = ['pdflatex', '-interaction', 'nonstopmode', filename+'.tex']
     process = subprocess.Popen(cmd)
     process.communicate()
@@ -42,26 +73,45 @@ def generate_pdf(filename):
     os.unlink(filename+'.log')
 
     return_code = process.returncode
+
     if not return_code == 0:
         os.unlink(filename+'.pdf')
         raise ValueError('Error {} executing command: {}'.format(return_code, ' '.join(cmd)))
+
+    else:
+        if open_files_flag:
+            os.startfile(filename+'.pdf')
 
 
 class MainGui(Frame):
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
+
         Label(self, text='Count of tasks:').grid(row=0, column=0)
         count = Entry(self)
         count.grid(row=0, column=1)
+        count.insert(0, '5')
+
         Label(self, text='Count of summands in one task:').grid(row=1, column=0)
         sum_cont = Entry(self)
         sum_cont.grid(row=1, column=1)
+        sum_cont.insert(0, '5')
+
         Label(self, text='Maximal degree:').grid(row=2, column=0)
         deg = Entry(self)
         deg.grid(row=2, column=1)
+        deg.insert(0, '5')
+
         Label(self, text='Coefficients dispersion:').grid(row=3, column=0)
         cof = Entry(self)
         cof.grid(row=3, column=1)
+        cof.insert(0, '5')
+
+        flag = Checkbutton(self, text='Open files after closing', variable=open_files_flag)
+        flag.grid(row=4, column=0)
+        # flag.edit_modified()
+        # open_files_flag = flag.getboolean(1)
+        # Label(self, text='Coefficients dispersion:').grid(row=4, column=1)
 
         Button(self, text='Add',
                command=(lambda: self.add(int(count.get()),
@@ -79,12 +129,9 @@ class MainGui(Frame):
         polygen_writer(count, sum_count, deg, cof)
 
 
-# noinspection PyRedeclaration
-problems = open("problems.tex", 'w')
-# noinspection PyRedeclaration
-answers = open("answers.tex", 'w')
-head_writer()
 print("Started")
+
+head_writer()
 
 if __name__ == '__main__':
     window = MainGui()
@@ -95,7 +142,7 @@ ground_writer()
 problems.close()
 answers.close()
 
-generate_pdf('answers')
-generate_pdf('problems')
+generate_pdf(answers_filename)
+generate_pdf(problems_filename)
 
 print("Finished")
